229dc265d6018a633845dcb9b00d0c949aecc52f
arine <ariangerris@gmail.com>
2024-08-27 16:25:51 +0400

    feat: calculate inventory total amount

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function calculateTotalAmount(executionContext) {
   const pricePerUnit = formContext
     .getAttribute("cr8c9_mon_price_per_unit")
     .getValue();
-
   let totalAmount = 0;
   if (quantity !== null && pricePerUnit !== null) {
     totalAmount = quantity * pricePerUnit;
@@ -51,24 +50,92 @@ async function calculateChildsQuantity(executionContext) {
   const form = executionContext.getFormContext();
   const recordId = form.data.entity.getId().replace(/[{}]/g, "");
   const fetchXML = `
-  <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
-    <entity name="cr8c9_inventory_product">
-      <attribute name="cr8c9_inventory_productid" />
-      <attribute name="cr8c9_int_quantity" />
-      <attribute name="cr8c9_fk_product_" />
-      <attribute name="cr8c9_fk_inventory" />
-      <attribute name="cr8c9_name" />
-      <order attribute="cr8c9_int_quantity" descending="false" />
-      <filter type="and">
-        <condition attribute="cr8c9_fk_inventory" operator="eq" value="${recordId}" />
-      </filter>
-    </entity>
-  </fetch>
-  `;
+<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+  <entity name="cr8c9_inventory_product">
+    <attribute name="cr8c9_inventory_productid" />
+    <attribute name="cr8c9_int_quantity" />
+    <attribute name="cr8c9_fk_product_" />
+    <attribute name="cr8c9_fk_inventory" />
+    <attribute name="cr8c9_name" />
+    <order attribute="cr8c9_int_quantity" descending="false" />
+    <filter type="and">
+      <condition attribute="cr8c9_fk_inventory" operator="eq" value="${recordId}" />
+    </filter>
+  </entity>
+</fetch>
+`;
   const result = await Xrm.WebApi.retrieveMultipleRecords(
     "cr8c9_inventory_product",
     "?fetchXml=" + encodeURIComponent(fetchXML)
   );
   const itemsQuantity = result.entities.length;
   form.getAttribute("cr8c9_int_items_quantity").setValue(itemsQuantity);
+}
+
+async function inventoryTotalPrice(executionContext) {
+  const form = executionContext.getFormContext();
+  const subgrid = form.getControl("inventory_products");
+  const priceListField = form.getAttribute("cr8c9_fk_price_list").getValue();
+  const priceListId = priceListField
+    ? priceListField[0].id.replace(/[{}]/g, "")
+    : null;
+
+  if (!priceListId) {
+    alert("Price List is not selected.");
+    return;
+  }
+
+  const fetchPrice = async (productId) => {
+    const fetchXml = `
+      <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+        <entity name="cr8c9_price_list_item">
+          <attribute name="cr8c9_price_list_itemid"/>
+          <attribute name="cr8c9_mon_price"/>
+          <order attribute="cr8c9_mon_price" descending="false"/>
+          <filter type="and">
+            <condition attribute="cr8c9_fk_product" operator="eq" value="{${productId}}"/>
+            <condition attribute="cr8c9_fk_price_list" operator="eq" value="${priceListId}"/>
+          </filter>
+        </entity>
+      </fetch>`;
+
+    const result = await Xrm.WebApi.retrieveMultipleRecords(
+      "cr8c9_price_list_item",
+      `?fetchXml=${encodeURIComponent(fetchXml)}`
+    );
+
+    if (result.entities.length > 0) {
+      return result.entities[0].cr8c9_mon_price || 0;
+    }
+    return 0;
+  };
+
+  let totalAmount = 0;
+
+  const rows = subgrid.getGrid().getRows();
+  for (let i = 0; i < rows.getLength(); i++) {
+    const row = rows.get(i);
+    const quantity =
+      row
+        .getData()
+        .getEntity()
+        .attributes.get("cr8c9_int_quantity")
+        .getValue() || 0;
+    const product = row
+      .getData()
+      .getEntity()
+      .attributes.get("cr8c9_fk_product_")
+      .getValue();
+    const productId = product ? product[0].id.replace(/[{}]/g, "") : null;
+
+    if (productId) {
+      const price = await fetchPrice(productId);
+      let lineTotalPrice = price * quantity;
+      if (quantity === 0) {
+        lineTotalPrice = price;
+      }
+      totalAmount += lineTotalPrice;
+    }
+  }
+  form.getAttribute("cr8c9_mon_total_amount").setValue(totalAmount);
 }
