446973632af189a087d887c583142e821752969c
arine <ariangerris@gmail.com>
2024-09-05 03:26:32 +0400

    feat: inventory popup

---
+++ js/inventory_popup/open_popup_js.js
@@ -0,0 +1,27 @@
+function openPopup(executionContext) {
+  const formContext = executionContext.getFormContext();
+  const inventoryId = formContext.data.entity.getId();
+  const data = JSON.stringify({
+    inventoryId: inventoryId
+  });
+
+  let pageInput = {
+    pageType: "webresource",
+    webresourceName: "cr8c9_inventory_popup_html",
+    data: data
+  };
+
+  let navigationOptions = {
+    target: 2,
+    width: 400,
+    height: 300,
+    position: 1
+  };
+
+  Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+    function success() {},
+    function error(e) {
+      console.error("Error: ", e.message);
+    }
+  );
+}
