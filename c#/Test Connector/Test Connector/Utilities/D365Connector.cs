3bc2ab4115236a205c239b96fba4e58738308ea6
arine <ariangerris@gmail.com>
2024-09-24 05:13:06 +0400

    issue fix

--- c#/Test Connector/Test Connector/Utilities/D365Connector.cs
+++ c#/Test Connector/Test Connector/Utilities/D365Connector.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.Xrm.Tooling.Connector;
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.Xrm.Tooling.Connector;
 
 namespace Test_Connector.Utilities
 {
-     class D365Connector
+    class D365Connector
     {
-       private static string clientId = "3840b01c-7b1c-4f37-9bdd-79a2a3722c86";
-       private static string clientSecret = "n458Q~Rb.tU0TsJMok.fALBIAJrHbS0ihrkAocxS";
-       private static string authority = "https://login.microsoftonline.com/91b242cb-658d-4278-bd71-2c38fde7444c";
-       private static string crmUrl = "https://org0f359662.crm4.dynamics.com/";
-       private static string conString = $"AuthType=ClientSecret;Url={crmUrl};ClientId={clientId};ClientSecret={clientSecret};Authority={authority};RequireNewInstance=True";
+        private static string clientId;
+        private static string clientSecret;
+        private static string authority;
+        private static string crmUrl;
+        static D365Connector()
+        {
+            LoadConfiguration();
+        }
+        private static void LoadConfiguration()
+        {
+                var json = File.ReadAllText("appsettings.json");
+                dynamic config = JsonConvert.DeserializeObject(json);
+                clientId = config.D365.ClientId;
+                clientSecret = config.D365.ClientSecret;
+                authority = config.D365.Authority;
+                crmUrl = config.D365.CrmUrl;  
+        }
         public static CrmServiceClient GetServiceClient()
         {
-            return new CrmServiceClient(conString);
+            return new CrmServiceClient($"AuthType=ClientSecret;Url={crmUrl};ClientId={clientId};ClientSecret={clientSecret};Authority={authority};RequireNewInstance=True");
         }
     }
 }
