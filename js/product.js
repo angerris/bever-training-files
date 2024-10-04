a61e9e06306d597bc72a1c558c293c679a2b0ca5
arine <ariangerris@gmail.com>
2024-10-05 01:30:06 +0400

    fix

--- js/product.js
+++ js/product.js
@@ -1,7 +1,11 @@
 function togglePricePerUnit(executionContext) {
   const formContext = executionContext.getFormContext();
-  const productType = 976090000 || 976090002;
+  const product = 976090000;
+  const assetProduct = 976090002;
+
   const isProductType =
-    formContext.getAttribute("cr8c9_os_type").getValue() === productType;
+    formContext.getAttribute("cr8c9_os_type").getValue() === product ||
+    formContext.getAttribute("cr8c9_os_type").getValue() === assetProduct;
+
   formContext.getControl("cr8c9_mon_price_per_unit").setVisible(isProductType);
 }
